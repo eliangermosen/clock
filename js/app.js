97c2af3fb591f50b9fc6e13a5898c539ace74074
elianmtg <elianmtoribio@gmail.com>
2022-08-01 11:51:09 -0400

    js and html complete

---
+++ js/app.js
@@ -0,0 +1,19 @@
+const $time = document.querySelector("h1");
+
+const currentTime = () =>{
+    
+    let date = new Date(),
+        hour = date.getHours(),
+        minute = date.getMinutes(),
+        second = date.getSeconds();
+
+    hour = hour < 10 ? `0${hour}` : hour;
+    minute = minute < 10 ? `0${minute}` : minute;
+    second = second < 10 ? `0${second}` : second;
+
+    let fullTime = `${hour}:${minute}:${second}`;
+    $time.innerText = fullTime;
+}
+
+currentTime();
+setInterval(currentTime, 1000);
